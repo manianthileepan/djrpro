6de62a700149a60deb0c026a3410fcf838437b79
manianthileepan <95533063+manianthileepan@users.noreply.github.com>
2022-07-23 17:36:43 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 song=""
+leftWristx=0;
+leftWristy=0;
+rightWristx=0;
+rightWristy=0;
+scoreleftwrist=0;
+scorerightwrist=0;
 function preload()
 {
     song=loadSound("music.mp3");
+    song2=loadSound("musicdemons.mpeg")
 }
-function play()
-
-    {
-        song.play();
-    }
-
-
 function setup(){
     canvas=createCanvas(600,500);
     canvas.center();
 
-    vidoe=createCapture(VIDEO);
+    video=createCapture(VIDEO);
     video.hide();
+     
+    poseNet=ml5.poseNet(video,modalloaded);
 }
-
+function play(){
+    song.play(musicdemons.mpeg);
+}
+function stop(){
+    song.stop();
+}
 function draw(){
     image(video,0,0,600,500);
-}
 
+   fill('#FF0000');
+   stroke("#FF0000");
+   
+if(scorerightwrist>0.2){
+    circle(rightWristx,rightWristy,20);
+    song.play(music.mp3);
+}
+if(scorerightwrist>0.2){
+    circle(rightWristx,rightWristy,20);
+    song.play(musicdemons.mpeg);
+}
+console.log(results)
+scorerightwrist=results[0].pose.keypoints[10].score;
+scoreleftwrist=results[0].pose.keypoints[9].score;
+console.log("scoreleftwrist="+scoreleftwrist + "scorerightrwrist="+scorerightwrist);}
